da8c655a34945e3624da1d7ef9e1346210d401e7
Hassan <apzhawk@gmail.com>
2020-05-25 15:08:54 +0800

    Update to Table,optimize

--- Userinfo.js
+++ Userinfo.js
@@ -17,11 +17,6 @@ export default class Userinfo extends Component {
         this.baseState = this.state;
     }
 
-    updateName = (e) => {
-        console.log(e.target.value)
-        this.setState({ name: e.target.value, id: 1 + Math.random() })
-    }
-
     updateState = (e) => {
         switch (e.target.name) {
             case "name":
@@ -54,11 +49,11 @@ export default class Userinfo extends Component {
                         this.setState({
                             skills: temp
                         });
-                      }
+                    }
                 }
                 break;
             default:
-                    alert("Error encountered")
+                alert("Error encountered")
         }
         this.setState({
             id: 1 + Math.random()
@@ -89,26 +84,16 @@ export default class Userinfo extends Component {
         }
     }
 
-    test = (e) => {
-        if (e.target.checked === true) {
-            const temp = [...this.state.skills];
-            if (!temp.includes(e.target.value)) {
-                temp.push(e.target.value)
-                this.setState({
-                    skills: temp
-                })
-            }
-        }
-        else if (e.target.checked === false) {
-            const temp = [...this.state.skills];
-            let index = temp.indexOf(e.target.value)
-            if (index !== -1) {
-                temp.splice(index, 1);
-                this.setState({
-                    skills: temp
-                });
-              }
-        }
+    personDetailTable = (person, index) => {
+        return (
+            <tr key={index}>
+                <td>{person.name}</td>
+                <td>{person.age}</td>
+                <td>{person.gender}</td>
+                <td>{person.about}</td>
+                <td>{person.skills}</td>
+            </tr>
+        )
     }
 
     render() {
@@ -153,42 +138,42 @@ export default class Userinfo extends Component {
                     </div>
                 </div>
 
-                <div className="checkbox-group required">
+
                 <div className="row">
                     <label htmlFor="fname">Programming</label>
-                    <input type="checkbox" name="box" onChange={this.updateState} value="Programming" name="skills"/>
+                    <input type="checkbox" name="box" onChange={this.updateState} value="Programming" name="skills" />
                 </div>
 
                 <div className="row">
                     <label htmlFor="fname">Gaming</label>
-                    <input type="checkbox" name="box" onChange={this.updateState} value="Gaming" name="skills"/>
+                    <input type="checkbox" name="box" onChange={this.updateState} value="Gaming" name="skills" />
                 </div>
 
                 <div className="row">
                     <label htmlFor="fname">Sleeping</label>
-                    <input type="checkbox" name="box" onChange={this.updateState} value="Sleeping" name="skills"/>
+                    <input type="checkbox" name="box" onChange={this.updateState} value="Sleeping" name="skills" />
                 </div>
-                </div>
-             
-                <button type="submit">Submit</button>
 
+                <button type="submit">Submit</button>
+               
                 <div className="row">
-                    <ul>
-                        {
-                            this.arr.map(item => {
-                                return (
-                                    <li key={item.id}>
-                                        {item.name}{' '}
-                                        {item.age}{' '}
-                                        {item.gender}{' '}
-                                        {item.skills}{' '}
-                                        {item.about}{' '}
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
+                    <br></br>
+                <table>
+                        <thead>
+                            <tr>
+                                <td>Name</td>
+                                <td>Age</td>
+                                <td>Gender</td>
+                                <td>About You</td>
+                                <td>Skills</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {this.arr.map(this.personDetailTable)}
+                        </tbody>
+                </table>
+               </div>
+          
             </form>
         )
     }
